e34de1018dc5f7356eb9a1ecd5c70d17278b636a
Tiramisu <135003824+Se7enseads@users.noreply.github.com>
2025-05-16 01:36:58 +0300

    :wrench: fix `wallpapers`

--- flake.nix
+++ flake.nix
@@ -9,13 +9,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
     in {
-      homeManagerModules.default = import ./home.nix self;
+      wallpapers.default = import ./home.nix;
 
       devShells.${system}.default = pkgs.mkShell {
         NIX_CONFIG = "extra-experimental-features = nix-command flakes";
